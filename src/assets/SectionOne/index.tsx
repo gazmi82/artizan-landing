f8370dfbe96966db9d2a1e5b6795ea28ef3f82f0
gazmir <gazmir.sulcaj@hinto.com>
2024-07-16 17:52:13 +0200

    setup

---
+++ src/assets/SectionOne/index.tsx
@@ -0,0 +1,9 @@
+import { default as Man } from "man.png";
+import { default as Card1 } from "Card1.png";
+import { default as Card2 } from "Card2.png";
+import { default as Card3 } from "Card3.png";
+import { default as Card4 } from "Card4.png";
+import { default as Card5 } from "Card5.png";
+import { default as Card6 } from "Card6.png";
+
+export const png = { Man, Card1, Card2, Card3, Card4, Card5, Card6 };
